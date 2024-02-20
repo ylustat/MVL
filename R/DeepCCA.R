4573dffd46127dc6e5bf521f90b8e747779b0f53
ylustat <yihaolu.ucas@gmail.com>
2024-02-19 20:14:54 -0600

    source first]

--- R/DeepCCA.R
+++ R/DeepCCA.R
@@ -3,6 +3,6 @@ DeepCCA <- function(X,Y,method="DVCCA", LATENT_DIMS = 2, EPOCHS = 100, lr = 0.00
   #   python_path <- reticulate::py_discover_config()$python
   # }
   # reticulate::use_python(python_path)
-  reticulate::source_python(paste0(system.file("python", package = "MVL", mustWork = TRUE),"/DeepCCA_base.py"))
+  # reticulate::source_python(paste0(system.file("python", package = "MVL", mustWork = TRUE),"/DeepCCA_base.py"))
   return(Deep_Models(X,Y,method="DVCCA", LATENT_DIMS = LATENT_DIMS, EPOCHS = EPOCHS, lr = lr, dropout = dropout, nw=nw))
 }
