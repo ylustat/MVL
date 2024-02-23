87f2ca9c3fc2f3459cfb25425aefd63c08927c9c
ylustat <yihaolu.ucas@gmail.com>
2024-02-22 23:04:57 -0600

    Update

--- R/variance_component_test.R
+++ R/variance_component_test.R
@@ -3,10 +3,9 @@ variance_component_test <- function(res){
   Q <- res$QRes %>% lapply(function(x) Reduce("+",x)/length(x))
   W <- res$WRes %>% lapply(function(x) Reduce("+",x)/length(x))
 
-  p <- mapply(function(q,w) {
-    tryCatch({
-      SKAT::Get_Davies_PVal(q, w)$p.value
-    }, Q, W, SIMPLIFY = F) %>%
+  p <- tryCatch({mapply(function(q,w) {
+    SKAT::Get_Davies_PVal(q, w)$p.value
+  }, Q, W, SIMPLIFY = F) %>%
       unlist()},silent = TRUE, error = function(x) return(NA))
   names(p) <- names(gammah)
   return(p)
